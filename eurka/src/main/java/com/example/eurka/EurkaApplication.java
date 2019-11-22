0fbba6b4e044e07a0ebf257571124751c0125712
hanh <hanhao@163.com>
2019-11-22 14:22:38 +0800

    init Git

---
+++ eurka/src/main/java/com/example/eurka/EurkaApplication.java
@@ -0,0 +1,14 @@
+package com.example.eurka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurkaApplication.class, args);
+    }
+
+}
