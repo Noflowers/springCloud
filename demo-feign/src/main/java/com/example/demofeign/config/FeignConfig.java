dfde8d2c0b26af4a9911ddf4ee2f18d1926879d5
hanh <hanhao@163.com>
2020-01-02 22:02:54 +0800

    提交 feign部分调试代码

---
+++ demo-feign/src/main/java/com/example/demofeign/config/FeignConfig.java
@@ -0,0 +1,11 @@
+package com.example.demofeign.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+//    @Bean
+//    public Retryer feignRetryer(){
+//        return new Retryer.Default(100, 1000,5);
+//    }
+}
--- demo-feign/src/main/java/com/example/demofeign/service/TestFeign.java
+++ demo-feign/src/main/java/com/example/demofeign/service/TestFeign.java
@@ -1,15 +1,15 @@
-package com.example.demofeign.service;
+    package com.example.demofeign.service;
 
-//import com.alibaba.fastjson.JSONObject;
-//import org.springframework.cloud.netflix.feign.FeignClient;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.RequestParam;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-//@FeignClient(name="SERVICE-PRODUCER")
+
+//@FeignClient(name="service-producer",configuration = FeignConfig.class)
 public interface TestFeign {
 
-//    @RequestMapping(value = "/hi",method = RequestMethod.GET)
-//    JSONObject getHi(@RequestParam("name") String name);
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    JSONObject getHi(@RequestParam("name") String name);
 
 }
